2090c1c5f13bd79242e8dd675bdb5a4f6eb5037a
John Van Sloun <jpvansloun@gmail.com>
2026-01-30 15:15:20 -0600

    modified scraper to support all difficulty modes

--- cypress/e2e/scraper.cy.ts
+++ cypress/e2e/scraper.cy.ts
@@ -5,13 +5,6 @@ interface PipState {
     cellRelations: string[]
 };
 
-const pipState: PipState = {
-    rows: 0,
-    cols: 0,
-    droppableCells: [],
-    cellRelations: []
-};
-
 function getCellColor(elem: JQuery<HTMLElement>): string {
     const COLOR_PATTERNS = [/purple/, /pink/, /orange/, /teal/, /blue/, /green/];
     const COLORS = ['PURPLE', 'PINK', 'ORANGE', 'TEAL', "BLUE", "GREEN"]; 
@@ -29,6 +22,66 @@ function getCellColor(elem: JQuery<HTMLElement>): string {
     return "NONE";
 }
 
+function getPipsData(mode: string) {
+    const pipState: PipState = {
+        rows: 0,
+        cols: 0,
+        droppableCells: [],
+        cellRelations: []
+    };
+
+    cy.get(`[data-testid="${mode.toUpperCase()}-toggle-button"]`).click();
+    cy.get('[data-testid="play-button"]').click();
+
+     // Using board container element styling to obtain dimensions
+    cy.get('[class^="Board-module_boardContainer_"]').then($el => {
+        const style = window.getComputedStyle($el[0]);
+        const rows = style.getPropertyValue('--rows');
+        const cols = style.getPropertyValue('--cols');
+
+        pipState.rows = parseInt(rows);
+        pipState.cols = parseInt(cols);
+    });
+
+    // Determining the droppable cells on the board
+    cy.get('[class^="Board-module_droppable_"]')
+        .children()
+        .each(($el) => {
+            const classes = [...$el[0].classList];
+
+            if (classes.includes("Board-module_hidden__DkSxz")) {
+                pipState.droppableCells.push(false);
+            } else {
+                pipState.droppableCells.push(true); 
+            }
+        });
+
+    // Gethers cell colors and constraints to determine relationships
+    cy.get('[class^="Board-module_regions_"]')
+        .children()
+        .each(($el) => {
+            if ($el.children().length == 0) {
+                pipState.cellRelations.push('NONE');
+            } else if ($el.children().length == 1) {
+                pipState.cellRelations.push(getCellColor($el));
+            } else {
+                let pipStateText = "";
+
+                pipStateText += getCellColor($el);
+
+                const constraintWrapper = $el.children()[1];
+                const constraint = constraintWrapper.children[0];
+
+                // "=" is implemented in a class whereas the other constraints are in element contained text
+                pipStateText += "_" + (constraint.textContent ? constraint.textContent : "=");
+
+                pipState.cellRelations.push(pipStateText);  
+            }
+        });
+
+    cy.writeFile(`data/pips_${mode}.json`, pipState);
+}
+
 describe('scraper', () => {
     it('scrapes pips game data', () => {
         cy.visit('https://www.nytimes.com/games/pips');
@@ -36,52 +89,16 @@ describe('scraper', () => {
         cy.get('[data-testid="play-button"]').click();
         cy.get('[data-testid="modal-close"]').click();
 
-        // Using board container element styling to obtain dimensions
-        cy.get('[class^="Board-module_boardContainer_"]').then($el => {
-            const style = window.getComputedStyle($el[0]);
-            const rows = style.getPropertyValue('--rows');
-            const cols = style.getPropertyValue('--cols');
+        cy.get('[data-testid="back-button"]').click();
 
-            pipState.rows = parseInt(rows);
-            pipState.cols = parseInt(cols);
-        });
+        getPipsData('easy');
 
-        // Determining the droppable cells on the board
-        cy.get('[class^="Board-module_droppable_"]')
-            .children()
-            .each(($el) => {
-                const classes = [...$el[0].classList];
+        cy.get('[data-testid="back-button"]').click();
+        
+        getPipsData('medium');
 
-                if (classes.includes("Board-module_hidden__DkSxz")) {
-                    pipState.droppableCells.push(false);
-                } else {
-                    pipState.droppableCells.push(true); 
-                }
-            });
+        cy.get('[data-testid="back-button"]').click();
 
-        // Gethers cell colors and constraints to determine relationships
-        cy.get('[class^="Board-module_regions_"]')
-            .children()
-            .each(($el) => {
-                if ($el.children().length == 0) {
-                    pipState.cellRelations.push('NONE');
-                } else if ($el.children().length == 1) {
-                    pipState.cellRelations.push(getCellColor($el));
-                } else {
-                    let pipStateText = "";
-
-                    pipStateText += getCellColor($el);
-
-                    const constraintWrapper = $el.children()[1];
-                    const constraint = constraintWrapper.children[0];
-
-                    // "=" is implemented in a class whereas the other constraints are in element contained text
-                    pipStateText += "_" + (constraint.textContent ? constraint.textContent : "=");
-
-                    pipState.cellRelations.push(pipStateText);  
-                }
-            });
-
-        cy.writeFile('data/pips_easy.json', pipState);
+        getPipsData('hard');
     });
 });
