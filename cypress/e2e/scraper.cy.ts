1c2952fca9c39d6ddb6b5864f26a5ede0f1b145d
John Van Sloun <johnvansloun@macbookpro.lan>
2026-01-29 23:30:58 -0600

    added functionality to track cell relationships

--- cypress/e2e/scraper.cy.ts
+++ cypress/e2e/scraper.cy.ts
@@ -2,23 +2,28 @@ interface PipState {
     rows: number,
     cols: number,
     droppableCells: boolean[]
+    cellRelations: string[]
 };
 
 const pipState: PipState = {
     rows: 0,
     cols: 0,
-    droppableCells: []
+    droppableCells: [],
+    cellRelations: []
 };
 
+const COLOR_PATTERNS = [/purple/, /pink/, /orange/, /teal/];
+const COLORS = ['PURPLE', 'PINK', 'ORANGE', 'TEAL'];
+
 describe('scraper', () => {
-    it('scraping pip data', () => {
+    it('scrapes pips game data', () => {
         cy.visit('https://www.nytimes.com/games/pips');
 
         cy.get('[data-testid="play-button"]').click();
         cy.get('[data-testid="modal-close"]').click();
 
         // Using board container element styling to obtain dimensions
-        cy.get('[class^="Board-module_boardContainer"]').then($el => {
+        cy.get('[class^="Board-module_boardContainer_"]').then($el => {
             const style = window.getComputedStyle($el[0]);
             const rows = style.getPropertyValue('--rows');
             const cols = style.getPropertyValue('--cols');
@@ -28,7 +33,7 @@ describe('scraper', () => {
         });
 
         // Determining the droppable cells on the board
-        cy.get('[class^="Board-module_droppable"]')
+        cy.get('[class^="Board-module_droppable_"]')
             .children()
             .each(($el) => {
                 const classes = [...$el[0].classList];
@@ -37,6 +42,42 @@ describe('scraper', () => {
                     pipState.droppableCells.push(false);
                 } else {
                     pipState.droppableCells.push(true); 
+                }
+            });
+
+        // Determining the relationships of cells
+        cy.get('[class^="Board-module_regions_"]')
+            .children()
+            .each(($el) => {
+                if ($el.children().length == 0) {
+                    pipState.cellRelations.push('NONE');
+                } else if ($el.children().length == 1) {
+                    const child = $el.children()[0];
+                    const classes = [...child.classList];
+                    
+                    for (let i = 0; i < COLOR_PATTERNS.length; i++) {
+                        if (classes.some(item => COLOR_PATTERNS[i].test(item))) {
+                            pipState.cellRelations.push(COLORS[i]); 
+                            break;
+                        }
+                    }
+                } else {
+                    let pipStateText = "";
+                    const child1 = $el.children()[0];
+                    const classes = [...child1.classList];
+
+                    for (let i = 0; i < COLOR_PATTERNS.length; i++) {
+                        if (classes.some(item => COLOR_PATTERNS[i].test(item))) {
+                            pipStateText += COLORS[i];
+                            break;
+                        }
+                    }
+
+                    const child2 = $el.children()[1];
+                    const granchild = child2.children[0];
+                    pipStateText += "_" + granchild.textContent;
+
+                    pipState.cellRelations.push(pipStateText);  
                 }
             });
     });
